15286c03e80f9f396c17526c0f954eac9915d658
Midland Computer <aharmankhan333@gmail.com>
2025-08-22 10:44:32 +0600

    update product detail section

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ProductDetailPage from './ProductDetail'
+import React from 'react';
+import ProductDetailPage from './ProductDetail';
 
-interface Props {
-  params: { id: string };
-}
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
 
+export default async function Page({ params }: PageProps) {
+  // await করতে হবে কারণ params এখন Promise
+  const { id } = await params;
 
+  const res = await fetch(`https://dummyjson.com/products/${id}`);
+  const data = await res.json();
 
-export default async function page({params:{id}}: Props) {
-    const res = await fetch(`https://dummyjson.com/products/${id}`)
-    const data = await res.json()
   return (
-   <div>
-  <ProductDetailPage productdata={data}></ProductDetailPage>
-   </div>
-  )
+    <div>
+      <ProductDetailPage productdata={data} />
+    </div>
+  );
 }
