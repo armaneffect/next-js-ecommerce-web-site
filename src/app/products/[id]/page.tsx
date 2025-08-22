a33c5ac74337fad36ff30a141fd5993f07fe5c34
Midland Computer <aharmankhan333@gmail.com>
2025-08-22 20:03:26 +0600

    added metadata

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -7,10 +7,39 @@ type PageProps = {
 };
 
 // --- Fetch product data ---
-async function getProductData(id: string) {
+async function getProductData(id: string): Promise<Product> {
   const res = await fetch(`https://dummyjson.com/products/${id}`);
   const data = await res.json();
   return data;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const product = await getProductData(params.id);
+
+  return {
+    title: product.title,
+    description: product.description,
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      url: `https://armaneffect.vercel.app/products/${params.id}`,
+      images: [
+        {
+          url: product.thumbnail || product.images[0],
+          width: 800,
+          height: 600,
+        },
+      ],
+      locale: 'en_US',
+      type: 'website', // ✅ product এর জায়গায় website ব্যবহার
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: product.title,
+      description: product.description,
+      images: [product.thumbnail || product.images[0]],
+    },
+  };
 }
 
 
